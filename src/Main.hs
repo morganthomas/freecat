cf0dafdf0ab28ee0ff142d2d3b10b29422b071fb
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2018-01-10 20:56:28 -0700

    show context as well as result

--- src/Main.hs
+++ src/Main.hs
@@ -15,9 +15,12 @@ main = do
       result <- runFreeCat (do
           context <- digestContext rawContext
           (expr, exprType) <- digestExpr context rawExpr
-          evaluate context expr
+          result <- evaluate context expr
+          return (context, result)
         )
       case result of
         Left err -> error (show err)
-        Right (result, st) -> putStrLn ("final result: " ++ show result)
+        Right ((context, result), st) -> do
+          putStrLn ("context:\n" ++ show context)
+          putStrLn ("final result: " ++ show result)
     _ -> error "wrong number of command line args, expected 2"
