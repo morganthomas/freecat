823e2d4fdbdd542659de41c3a0f2924e442444a6
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2018-02-19 17:07:28 -0700

    mark out test cases

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo "01-list"
 ./dist/build/freecat/freecat test/cases/01-list/context.fcat example
+echo "02-list-implicit"
 ./dist/build/freecat/freecat test/cases/02-list-implicit/context.fcat example
+echo "03-list-partially-implicit"
 ./dist/build/freecat/freecat test/cases/03-list-partially-implicit/context.fcat example
