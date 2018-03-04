d0fe824cb08fdc806631537b46613c03e46f1809
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2018-03-04 01:17:46 -0700

    make inferOuterImplicitArguments less broken

--- src/FreeCat/Digest.hs
+++ src/FreeCat/Digest.hs
@@ -270,7 +270,7 @@ inferOuterImplicitArguments rawE c e t et = do
    (argsToInfer, codomain) <- findMatchingImplicitCodomain t et
    c' <- unifyExprWithExpr rawE c (et, codomain)
    inferredArgs <- mapM (evaluate c') (Prelude.map (\sym -> SymbolExpr sym Nothing) argsToInfer)
-   return (Prelude.foldr (\e' arg -> ImplicitAppExpr e' arg Nothing) e inferredArgs)
+   return (Prelude.foldr (\arg e' -> ImplicitAppExpr e' arg Nothing) e inferredArgs)
 
 findMatchingImplicitCodomain :: Expr -> Expr -> FreeCat ([Symbol], Expr)
 findMatchingImplicitCodomain e matches =
