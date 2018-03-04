fe890095f2e2baa62149fdd0c8214c093571ef9d
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2018-03-04 01:19:20 -0700

    refactor

--- src/FreeCat/Digest.hs
+++ src/FreeCat/Digest.hs
@@ -269,7 +269,7 @@ inferOuterImplicitArguments :: RawExpr -> Context -> Expr -> Expr -> Expr -> Fre
 inferOuterImplicitArguments rawE c e t et = do
    (argsToInfer, codomain) <- findMatchingImplicitCodomain t et
    c' <- unifyExprWithExpr rawE c (et, codomain)
-   inferredArgs <- mapM (evaluate c') (Prelude.map (\sym -> SymbolExpr sym Nothing) argsToInfer)
+   inferredArgs <- mapM (evaluate c' . (\sym -> SymbolExpr sym Nothing)) argsToInfer
    return (Prelude.foldr (\arg e' -> ImplicitAppExpr e' arg Nothing) e inferredArgs)
 
 findMatchingImplicitCodomain :: Expr -> Expr -> FreeCat ([Symbol], Expr)
